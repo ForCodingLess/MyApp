4110e214cf40f49b1fbb0641f386ec91ba1c6cb1
ForCodingLess <l_beauty@163.com>
2018-01-18 22:31:47 +0800

    -a

--- App.js
+++ App.js
@@ -4,12 +4,12 @@ import { StyleSheet, Text, View,Image} from 'react-native';
 export default class App extends React.Component {
   render() {
     return (
-      <ScrollView style={styles.container}>
+      <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
         <Image style={{width:40,height:40,marginTop:60}} source={{uri:'https://shp.qpic.cn/bizmp/CQ3lYf0saco7sJYbFrn3jvMxDJHjqZqiaqtGMtMNSqnW6DwcRD6qwGA/'}}/>
-      </ScrollView>
+      </View>
     );
   }
 }
